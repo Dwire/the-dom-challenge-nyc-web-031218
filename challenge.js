9707439e0e330b7f7a17c8fbd09a31c27f8f1230
Dwire <gjdwyer8@gmail.com>
2018-04-27 11:58:52 -0400

    Finish Counter

--- challenge.js
+++ challenge.js
@@ -4,10 +4,15 @@ const plus = document.getElementById("+")
 const like = document.getElementById("<3")
 const pause = document.getElementById("pause")
 
+let count = 0;
 function counterInt() {
-  parseInt(counter.innerHTML)++
+  count++
+  return counter.innerHTML = count.toString()
+
 }
 
 (function() {
   setInterval(counterInt, 1000)
 })()
+
+// return counter.innerHTML = (parseInt(counter.innerHTML) + count).toString()
