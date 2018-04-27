5c5bbe45679c3916207af62c7bb8a657c04fffec
Dwire <gjdwyer8@gmail.com>
2018-04-27 12:37:53 -0400

    add and minus functions

--- challenge.js
+++ challenge.js
@@ -4,15 +4,25 @@ const plus = document.getElementById("+")
 const like = document.getElementById("<3")
 const pause = document.getElementById("pause")
 
+
 let count = 0;
 function counterInt() {
   count++
   return counter.innerHTML = count.toString()
-
 }
 
-(function() {
-  setInterval(counterInt, 1000)
-})()
+let iffy = setInterval(counterInt, 1000)
 
-// return counter.innerHTML = (parseInt(counter.innerHTML) + count).toString()
+// (function() {
+//   setInterval(counterInt, 1000)
+// })()
+
+minus.addEventListener("click", function() {
+  // clearInterval(iffy)
+  count -= 1
+  }
+)
+plus.addEventListener("click", function() {
+  counterInt()
+  }
+)
